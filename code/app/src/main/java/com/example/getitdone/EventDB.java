7e1f05a4de84978db94f356af58be7a004f41686
MasonUWF <mr108@students.uwf.edu>
2020-04-21 17:12:15 -0500

    db changes

--- code/app/src/main/java/com/example/getitdone/EventDB.java
+++ code/app/src/main/java/com/example/getitdone/EventDB.java
@@ -7,14 +7,14 @@ import java.sql.SQLException;
 public class EventDB {
 
     public static Connection getRemoteConnection() {
-        if (System.getenv("qedb.cmkzamfpzfbh.us-east-2.rds.amazonaws.com:4848") != null) {
+        if (System.getenv("phoningitin.cmkzamfpzfbh.us-east-2.rds.amazonaws.com:3306") != null) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
                 String dbName = System.getenv("GetItDoneDB");
                 String userName = System.getenv("admin");
-                String password = System.getenv("P@S$W0RD");
-                String hostname = System.getenv("qedb.cmkzamfpzfbh.us-east-2.rds.amazonaws.com");
-                String port = System.getenv("4848");
+                String password = System.getenv("password");
+                String hostname = System.getenv("phoningitin.cmkzamfpzfbh.us-east-2.rds.amazonaws.com");
+                String port = System.getenv("3306");
                 String jdbcUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
                 Connection con = DriverManager.getConnection(jdbcUrl);
                 return con;
